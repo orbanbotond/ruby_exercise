a8ee5ef9583567c7a8f9ba298d299c1a802d9417
Orban Botond <orbanbotond@gmail.com>
2015-03-18 22:35:50 +0200

    Permutation check is done

---
+++ spec/alg_counting_elements_01_perm_check_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+#O(n)
+#space O(n)
+def solution(a)
+  return 0 if a.max > a.size
+  b = Array.new a.max, 0
+  a.each do |elem|
+    b[elem-1] += 1
+  end
+  b.all?{|x|x==1} ? 1:0
+end
+
+describe 'Tape Equilibrum' do
+
+  specify 'normal case' do
+    expect(solution([4,1,3,2])).to eq(1)
+  end
+
+  specify 'negative case' do
+    expect(solution([4,1,3])).to eq(0)
+  end
+
+end
