55eb8c23a1729550b5123bc9592d925cdd31914b
Orban Botond <orbanbotond@gmail.com>
2015-03-19 17:28:17 +0200

    Algorythm for intersecting circles is now ok

--- spec/alg_sorting_04_intersecting_circles_spec.rb
+++ spec/alg_sorting_04_intersecting_circles_spec.rb
@@ -2,25 +2,41 @@
 # puts "this is a debug message"
 
 def solution(a)
-#    axis_intersections = a.map.with_index{|x, idx| [ idx - x, idx + x ] }
-    axis_intersections = []
-    intersecting_circles_count = 0
-    a.each.with_index do |radius, idx|
-        left_intersection = idx - radius
-        right_intersection = idx + radius
-        center = idx
-        axis_intersections.each do |intersection|
-            intersecting_circles_count +=1 if  center == intersection.first || center == intersection.last || left_intersection <= intersection.last
-            
-        end
-        axis_intersections << [left_intersection, right_intersection]
+    end_points = []
+    a.each_with_index do |ai, i|
+        end_points << [i - ai, i + ai]
     end
-    intersecting_circles_count
-    # write your code in Ruby 2.2
+    end_points = end_points.sort_by { |points| points[0]}
+
+    intersecting_pairs = 0
+    end_points.each_with_index do |point, index|
+        lep, right_ep = point
+        pairs = bsearch(end_points, index, end_points.size - 1, right_ep)
+        return -1 if 10000000 - pairs + index < intersecting_pairs
+        intersecting_pairs += (pairs - index)
+    end
+    return intersecting_pairs
 end
 
-#TODO
-# Detected time complexity: O(N**3)
+def bsearch(a, l, u, x)
+    if l == u
+        if x >= a[u][0]
+            return u
+        else
+            return l - 1 
+        end
+    end
+    mid = (l + u)/2
+
+    # Notice that we are searching in higher range
+    # even if we have found equality.
+    if a[mid][0] <= x
+        return bsearch(a, mid+1, u, x)
+    else
+        return bsearch(a, l, mid, x)
+    end
+end
+
 describe 'Detect intersecting circles' do
   #TODO
   #check the performance again
