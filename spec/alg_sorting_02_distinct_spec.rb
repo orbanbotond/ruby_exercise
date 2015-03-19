b74bf81cca3daa4b362f6ac38707a237df8cf6c1
Orban Botond <orbanbotond@gmail.com>
2015-03-19 13:33:38 +0200

    Distinct count is 100% with both methods

--- spec/alg_sorting_02_distinct_spec.rb
+++ spec/alg_sorting_02_distinct_spec.rb
@@ -1,13 +1,29 @@
-def solution(a)
+
+def solution1(a)
     a.uniq.size
 end
+
+def solution2(a)
+    distinct_count = 0
+    d = {}
+    a.each do |x|
+        unless d[x]
+            d[x] = x
+            distinct_count += 1
+        end
+    end
+
+    distinct_count
+end
+
+alias :solution :solution2
 
 require 'spec_helper'
 
 describe 'Distinct' do
 
   specify 'normal case' do
-    expect(solution([-3, 1, 2, -2, 5, 6])).to eq(60)
+    expect(solution([2,2,1,3,1,1])).to eq(3)
   end
 
 end
