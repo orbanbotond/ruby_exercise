d1c218973c5b2517c27a8d1633e98dd1fbe61ce6
Orban Botond <orbanbotond@gmail.com>
2015-03-22 21:17:57 +0200

    Corrected to 100%

--- spec/alg_prefix_sums_02_count_divs_spec.rb
+++ spec/alg_prefix_sums_02_count_divs_spec.rb
@@ -1,7 +1,14 @@
 # you can use puts for debugging purposes, e.g.
 # puts "this is a debug message"
 
+def do_debug
+  # debugging = true
+  debugging = false
+  yield if debugging
+end
+
 def solution(a, b, k)
+    do_debug{ puts "a:#{a} | b:#{b} | k:#{k}"}
     d1 = a / k
     d1+=1 if d1*k < a
     d2 = b / k
