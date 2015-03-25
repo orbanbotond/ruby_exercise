e50311079aba13fe5d87fc6f1cc5864836f53478
Orban Botond <orbanbotond@gmail.com>
2015-03-25 22:31:33 +0200

    Fibonacci frog jump is at 33%

---
+++ spec/alg_fibonacci_02_frog_jump_spec.rb
@@ -0,0 +1,85 @@
+# you can use puts for debugging purposes, e.g.
+# puts "this is a debug message"
+
+def generate_fibonacci(max)
+  array = Array.new
+  array << 0
+  array << 1
+  while array.last < max do
+    array << array[-1] + array[-2]
+  end
+  array
+end
+
+def generate_prefix(a)
+  a.map.with_index do |x,idx|
+    if x == 1
+      idx +1 
+    else
+      0
+    end
+  end
+end
+
+def solution(a)
+  # puts "a:#{a}"
+  a << 1
+  prefix = generate_prefix a
+  # puts "prefix:#{prefix}"
+  fib = generate_fibonacci(prefix.max)
+  # puts "fib:#{fib}"
+
+  last = 0
+  jumps = 0
+  while last <= prefix.size - 1
+    # puts "last:#{last} prefix:#{prefix.size - 1} checking:#{last < prefix.size - 1} jumps:#{jumps}"
+    found = false
+    prefix.size.downto(last) do |n|
+      position = prefix[n-1]
+      # puts "idx:#{n} position:#{position} last:#{last} n-last:#{position-last} isfib?:#{fib.any?{|x| x == position - last }}"
+      if fib.any?{|x| x == position - last } && position > 0
+        # puts "found a jumping position. jumping"
+        jumps += 1
+        last = n
+        found = true
+        break
+      end
+    end
+    unless found
+      # puts "No jumping position is found! return -1"
+      return -1
+    end
+  end
+  jumps
+end
+
+require 'spec_helper'
+
+describe 'Frog Jumps' do
+
+  specify 'extreme small' do
+    expect(solution([])).to eq(1)
+  end
+
+  specify 'simple' do
+    expect(solution([0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0])).to eq(3)
+  end
+
+  specify 'simple' do
+    expect(solution([0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1])).to eq(3)
+  end
+
+  specify 'ex1' do
+    expect(solution([1,1,1,0,1,0,0,1,0,0,1,0,1,0,0])).to eq(2)
+  end
+
+  specify 'ex2' do
+    expect(solution([1,1,1,0,1,0,0,1,0,0,1,1,0,0,0])).to eq(2)
+  end
+
+  specify 'small cyclic' do
+    expect(solution([0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1])).to eq(3)
+  end
+
+end
+
