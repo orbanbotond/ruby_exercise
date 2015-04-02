02fb1f01c4fd093934fa087e81c668234c0d7cda
Orban Botond <orbanbotond@gmail.com>
2015-04-02 11:18:17 +0300

    The solution is 100% with caterpillar method

--- spec/alg_caterpillar_04_min_abs_sum_of_two_spec.rb
+++ spec/alg_caterpillar_04_min_abs_sum_of_two_spec.rb
@@ -1,7 +1,7 @@
 # you can use puts for debugging purposes, e.g.
 # puts "this is a debug message"
 
-def solution(a)
+def solution_2(a)
   # puts "a:#{a}"
   a.map!{|x| [x.abs, x < 0 ? -1 : 1] }
   a.sort!{|x,y| x.first <=> y.first }
@@ -18,24 +18,51 @@ def solution(a)
   # write your code in Ruby 2.2
 end
 
+def solution(a)
+  a.sort!
+
+  return (a[-1] + a[-1]).abs if a[-1] <= 0
+  return a[0] + a[0] if a[0] >= 0
+
+  back, front = 0, a.size - 1
+
+  min = a[-1] + a[-1]
+
+  while back <= front do
+    temp = (a[back] + a[front]).abs
+    min = temp if temp < min
+    if (a[back] + a[front -1]).abs <= temp
+      front -= 1
+    elsif (a[back + 1] + a[front]).abs <= temp
+      back += 1
+    else
+      front -= 1
+      back += 1
+    end
+  end
+
+  min
+  # write your code in Ruby 2.2
+end
+
 require 'spec_helper'
 
 describe 'min abs sum of two' do
 
   specify 'normal case' do
-    expect(solution([1, 4, -3, 5] )).to eq(1)
+    expect(solution([1, 4, -3, 5])).to eq(1)
   end
 
   specify 'normal case' do
-    expect(solution([1, 4, -3, -6, 8] )).to eq(1)
+    expect(solution([1, 4, -3, -6, 8])).to eq(1)
   end
 
   specify 'normal case' do
-    expect(solution([-8, 8, -5, 2] )).to eq(0)
+    expect(solution([-8, 8, -5, 2])).to eq(0)
   end
 
   specify 'normal' do
-    expect(solution([-8, -4, -5, -1] )).to eq(2)
+    expect(solution([-8, -4, -5, -1])).to eq(2)
   end
 
   specify 'normal' do
@@ -43,15 +70,15 @@ describe 'min abs sum of two' do
   end
 
   specify 'normal' do
-    expect(solution([-1000000000, -999999999] )).to eq(1999999998)
+    expect(solution([-1000000000, -999999999])).to eq(1999999998)
   end
 
   specify 'all positive' do
-    expect(solution([8, 5, 3, 4, 6, 8] )).to eq(6)
+    expect(solution([8, 5, 3, 4, 6, 8])).to eq(6)
   end
 
   specify 'all negative' do
-    expect(solution([-8, -5, -4, -10, -12, -18] )).to eq(8)
+    expect(solution([-8, -5, -4, -10, -12, -18])).to eq(8)
   end
 
 end
