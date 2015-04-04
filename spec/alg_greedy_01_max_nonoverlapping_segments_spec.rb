d1ecfa18070e38fccb7ae0e6a137ccdf8db25cbc
Orban Botond <orbanbotond@gmail.com>
2015-04-04 14:51:13 +0300

    non  overlapping segments at 100%

---
+++ spec/alg_greedy_01_max_nonoverlapping_segments_spec.rb
@@ -0,0 +1,48 @@
+# you can use puts for debugging purposes, e.g.
+# puts "this is a debug message"
+
+def overlap(first_segment, last_segment)
+  # puts "f:#{first_segment} l:#{last_segment}"
+  if first_segment.first <= last_segment.first &&
+    last_segment.first <= first_segment.last ||
+    last_segment.first <= first_segment.first &&
+    first_segment.first <= last_segment.last
+    ret = true
+  else
+    ret = false
+  end
+  # puts "r:#{ret}"
+  ret
+end
+
+def solution(a, b)
+  c = [a,b].transpose
+  # puts "c:#{c}"
+  counted = []
+
+  c.each do |segment|
+    last = counted.last
+    next if last && overlap(last, segment)
+    counted << segment
+    # puts "c:#{counted}"
+  end
+  counted.size
+end
+
+require 'spec_helper'
+
+describe 'min abs sum of two' do
+
+  specify 'normal case' do
+    expect(solution([1, 3, 7, 9, 9], [5, 6, 8, 9, 10])).to eq(3)
+  end
+
+  specify 'empty' do
+    expect(solution([], [])).to eq(0)
+  end
+
+  specify 'every second' do
+    expect(solution([11,22,33,44,55], [22,33,44,55,66])).to eq(3)
+  end
+
+end
