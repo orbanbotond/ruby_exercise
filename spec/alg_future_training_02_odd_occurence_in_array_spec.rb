6bdb4a5c00cc1ea1e973aa4c413569c608a4b2ab
Orban Botond <orbanbotond@gmail.com>
2015-04-08 12:15:13 +0300

    Odd occurences are detected in the array at 100%

---
+++ spec/alg_future_training_02_odd_occurence_in_array_spec.rb
@@ -0,0 +1,20 @@
+
+def solution(a)
+  pairs = {}
+  a.each do |x|
+    pairs[x] = pairs.fetch(x){0} + 1
+  end
+
+  odd = pairs.find{|x|x.last.odd?}
+  odd.first
+end
+
+require 'spec_helper'
+
+describe 'Tree depth' do
+
+  specify 'extreme small ones' do
+    expect(solution([9, 3, 9, 3, 9, 7, 9])).to eq(7)
+  end
+
+end
