fa8f672b427936f37adf67ef6bde50c176e2fd92
Orban Botond <orbanbotond@gmail.com>
2015-04-10 14:03:17 +0300

    Missing permutation takes O(n) space now

--- spec/alg_time_complexity_02_perm_missing_elem.rb
+++ spec/alg_time_complexity_02_perm_missing_elem.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 # you can use puts for debugging purposes, e.g.
 # puts "this is a debug message"
 # O(n)
-# O(1)
+# O(n)
 #
-def solution(a)
+def solution_o_n_space(a)
   space = Array.new a.size + 1, 0
   a.each do |x|
     space[x-1] = 1
@@ -13,9 +13,25 @@ def solution(a)
   space.find_index(0) + 1
 end
 
+def solution(a)
+  sum = a.reduce(:+)
+  sum = 0 if sum.nil?
+  size = a.size
+  desired_sum = ((size + 1) * (size + 2))/2
+  desired_sum - sum
+end
+
 require 'spec_helper'
 
 describe 'Perm missing element' do
+
+  specify 'single array' do
+    expect(solution([2])).to eq(1)
+  end
+
+  specify 'empty list' do
+    expect(solution([])).to eq(1)
+  end
 
   specify 'simple 1' do
     expect(solution([1,2,4])).to eq(3)
