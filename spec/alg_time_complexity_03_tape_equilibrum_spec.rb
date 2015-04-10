4c2835e225a921dbaba1c48857ecee43a3f387dc
Orban Botond <orbanbotond@gmail.com>
2015-04-10 18:22:16 +0300

    A nicer alg is added

--- spec/alg_time_complexity_03_tape_equilibrum_spec.rb
+++ spec/alg_time_complexity_03_tape_equilibrum_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 #O(n)
 #space O(n)
-def solution(a)
+def solution_ugly(a)
   sum = a.reduce(:+)
   b = Array.new a.size + 1, 0
   min = nil
@@ -20,6 +20,28 @@ def solution(a)
   min
 end
 
+def solution(a)
+  return a.first if a.size == 1
+  max = a.reduce(:+)
+
+  prefix = [0]
+  a.each do |x|
+    prefix << prefix.last + x
+  end
+  prefix.shift
+
+  min = nil
+
+  prefix[0..-2].each do |x|
+    current_diff = (x - max + x).abs
+    if min.nil? || min != nil && current_diff < min
+      min = current_diff
+    end
+  end
+  min
+end
+
+
 # 13
 # [3,1,2,4,3]
 # [0,0,0,0,0]
@@ -29,6 +51,10 @@ describe 'Tape Equilibrum' do
 
   specify 'normal case' do
     expect(solution([3,1,2,4,3])).to eq(1)
+  end
+
+  specify 'single element array' do
+    expect(solution([1])).to eq(1)
   end
 
 end
