6d25add347906b99835367c6dc3566f9d2e236b0
Orban Botond <orbanbotond@gmail.com>
2015-04-13 15:43:06 +0300

    Having a nicer solution to The passing cars.

--- spec/alg_prefix_sums_01_passing_cars_spec.rb
+++ spec/alg_prefix_sums_01_passing_cars_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
+def solution_nicer(a)
+  counter = 0
+  incrementer = 0
+  a.each do |x|
+    incrementer += 1 if x == 0
+    counter += incrementer if x == 1
+    return -1 if counter > 1000000000
+  end
+  counter
+end
+
 def solution(a)
     west_count = 0
     east_count = 0
