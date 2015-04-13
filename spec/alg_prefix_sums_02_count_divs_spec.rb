e9658981435a8eebc0ffd8a6d81830f54639ea8e
Orban Botond <orbanbotond@gmail.com>
2015-04-13 16:26:57 +0300

    A nicer solution for the count div is found

--- spec/alg_prefix_sums_02_count_divs_spec.rb
+++ spec/alg_prefix_sums_02_count_divs_spec.rb
@@ -7,13 +7,18 @@ def do_debug
   yield if debugging
 end
 
-def solution(a, b, k)
+def solution_s(a, b, k)
     do_debug{ puts "a:#{a} | b:#{b} | k:#{k}"}
     d1 = a / k
     d1+=1 if d1*k < a
     d2 = b / k
     d2 - d1 + 1
     # write your code in Ruby 2.2
+end
+
+def solution(a, b, k)
+  edge = (a % k == 0 )? 1 : 0
+  return b/k - a/k + edge
 end
 
 require 'spec_helper'
