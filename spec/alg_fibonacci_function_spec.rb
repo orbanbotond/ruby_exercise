7b4dde2bbaff455fbbc3ccdb9ef813106df66cbf
Orban Botond <orbanbotond@gmail.com>
2015-04-29 09:47:53 +0300

    The tricky function which is reduced two the last two fibonacci sequence.

---
+++ spec/alg_fibonacci_function_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+def last_two_fibonacci(steps)
+  prev_0 = 0
+  prev_1 = 1
+  steps.times do
+    prev_0, prev_1 = [prev_1, prev_1 + prev_0]
+  end
+  [prev_0, prev_1]
+end
+
+def solution(a, b, n)
+  fib_1, fib2 = last_two_fibonacci(n + 1)
+  a*fib2 + b*fib_1
+end
+
+describe 'Fibonacci multiplied functions' do
+
+  specify 'unit 1_1' do
+    expect(solution(1, 2, 5)).to eq(29)
+  end
+
+end
