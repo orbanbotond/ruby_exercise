0776d937267679119913392144d709f3b9a521b3
Orban Botond <orbanbotond@gmail.com>
2017-02-17 02:02:01 +0200

    The scope gates are inspected well. :)

---
+++ spec/meta_programming/binding_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe 'Binding' do
+  context 'basic binding' do
+    specify 'grab the binding at the moment it is defined.' do
+      def my_method
+        x = "Goodbye"      
+        yield("cruel" ) 
+      end
+      x = "Hello"
+      expect(my_method {|y| "#{x}, #{y} world" }).to eq('Hello, cruel world')
+    end
+  end
+
+  context 'binding dissapearance' do
+    specify 'it dissapears after the block' do
+      def my_method
+        yield
+      end
+
+      top_level_variable = 1 
+      my_method do
+        top_level_variable += 1
+        local_to_block = 1
+      end
+      expect(top_level_variable).to eq(2)
+      expect { local_to_block }.to raise_error(NameError)
+    end
+  end
+
+  context 'scope inspection' do
+    specify '' do
+      v1 = 1
+      class MyClass
+        #since class is a 'scope gate' the rspec expectation methods need to be included and extended
+        include ::RSpec::Matchers
+        extend ::RSpec::Matchers
+        v2 = 2
+
+        expect(local_variables).to eq([:v2])
+        def my_method 
+          v3 = 3
+
+          expect(local_variables).to eq([:v3])
+        end
+        expect(local_variables).to eq([:v2])
+      end
+      obj = MyClass.new
+      obj.my_method
+      obj.my_method
+      expect(local_variables).to eq([:v1, :obj])
+    end
+  end
+end
