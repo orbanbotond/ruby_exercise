5e35785a2306859b064f6fcadd54aac289f3e1c4
Orban Botond <orbanbotond@gmail.com>
2017-02-23 13:04:57 +0200

    Another map which is side effect less and non recursive.

--- spec/functional/map_spec.rb
+++ spec/functional/map_spec.rb
@@ -12,12 +12,20 @@ describe 'Map' do
     [ yield(array[0]) ] + map_recursive(array[1..-1], &block)
   end
 
+  def map_side_effect_less_non_recursive(array, &block) 
+    array.inject([]) { |accumulator, iterated| [ yield(iterated) ] + accumulator }
+  end
+
   specify 'simple' do
     expect(map_simple([1,2,3]){|x| 2 * x}).to eq([2,4,6])
   end
 
   specify 'side effect less map' do
     expect(map_recursive([1,2,3]){|x| 2 * x}).to eq([2,4,6])
+  end
+
+  specify 'side effect less map non recursive' do
+    expect(map_side_effect_less_non_recursive([1,2,3]){|x| 2 * x}).to eq([2,4,6])
   end
 
   specify 'recursive map take more time x1.6' do
@@ -28,6 +36,16 @@ describe 'Map' do
         1000000.times { map_recursive([1,2,3]){|x| 2 * x} }
     end
     expect(b2.total / b1.total).to be_within(0.5).of(1.6)
+  end
+
+  specify 'recursive vs non recursive' do
+    b1 = Benchmark.measure do
+        1000000.times { map_side_effect_less_non_recursive([1,2,3]){|x| 2 * x} }
+    end
+    b2 = Benchmark.measure do
+        1000000.times { map_recursive([1,2,3]){|x| 2 * x} }
+    end
+    expect(b2.total / b1.total).to be_within(0.5).of(1.0)
   end
 
   specify 'recursive map takes the same space' do
