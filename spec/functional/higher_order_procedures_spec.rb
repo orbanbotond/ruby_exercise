920df0d997cb3cd65997064d099567563e812875
Orban Botond <orbanbotond@gmail.com>
2017-02-25 07:24:48 +0200

    Higher Order functions are added :)

---
+++ spec/functional/higher_order_procedures_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe 'Higher Order Procedures' do
+  context '&obj calls the proc on the object' do
+    context 'symbol' do
+      class Symbol
+        def to_proc
+          lambda { |x| x.send(self) } 
+        end
+      end
+      specify 'simple' do
+        expect( %w[foo bar baz].map(&:capitalize)).to eq(['Foo', 'Bar', 'Baz'])
+      end
+    end
+    context 'bigger object' do
+      class Filter
+        def initialize
+          @constraints = []
+        end
+        def constraint(&block)
+          @constraints << block
+        end
+        def to_proc
+          lambda { |e| @constraints.all? { |fn| fn.call(e) } }
+        end
+      end
+      specify "nice is'n it" do
+        filter = Filter.new 
+        filter.constraint { |x| x > 10 }
+        filter.constraint { |x| x.even? }
+        filter.constraint { |x| x % 3 == 0 }
+        expect((8..24).select(&filter)).to eq([12,18,24])
+        filter.constraint { |x| x % 4 == 0 }
+        expect((8..24).select(&filter)).to eq([12,24])
+      end
+    end
+  end
+end
