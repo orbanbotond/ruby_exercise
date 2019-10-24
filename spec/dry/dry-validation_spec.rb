bbc6ebadbf8c7c2deb04e3962ee006c9711ce4d6
Orban Botond <orbanbotond@gmail.com>
2019-10-24 15:00:15 +0300

    Added more validation rules.

---
+++ spec/dry/dry-validation_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'Dry-Validation' do
+  describe 'just schema' do
+  end
+
+  describe 'rules' do
+    context 'rule dependencies' do
+    end
+  end
+
+  describe 'custom predicates' do
+  end
+
+  describe 'strict key presence' do
+  end
+end
