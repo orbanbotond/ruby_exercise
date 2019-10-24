ffac4e0337aa7acd31392ca9e544fd2e0cc18c79
Orban Botond <orbanbotond@gmail.com>
2019-10-24 14:24:10 +0300

    Add hash test.

--- spec/dry/dry-schema_spec.rb
+++ spec/dry/dry-schema_spec.rb
@@ -163,6 +163,114 @@ describe 'Dry-Schema' do
   end
 
   describe 'type checking' do
+    context 'simple types' do
+      subject(:schema_validation) {
+        schema = Dry::Schema.Params do
+          required(:name).filled(:string)
+        end
+
+        schema.call params
+      }
+
+      context 'negative' do
+        context 'wrong type is passed as value' do
+          let(:params) { { name: 2 } }
+
+          it { should be_failure }
+
+          it "the errors should point out the missing key" do
+            expect(schema_validation.errors.to_h).to include(:name)
+          end
+        end
+      end
+
+      context 'positive' do
+        context 'present key' do
+          let(:params) { {name: 'value' } }
+
+          it { should be_success }
+
+          it "the errors should be empty" do
+            expect(schema_validation.errors).to be_empty
+          end
+        end
+      end
+    end
+
+    describe 'arrays' do
+      subject(:schema_validation) {
+        schema = Dry::Schema.Params do
+          required(:names).array(:string)
+        end
+
+        schema.call params
+      }
+
+      context 'negative' do
+        context 'wrong type is passed as value' do
+          let(:params) { { names: 'asd' } }
+
+          it { should be_failure }
+
+          it "the errors should point out the missing key" do
+            expect(schema_validation.errors.to_h).to include(:names)
+          end
+        end
+      end
+
+      context 'positive' do
+        context 'present key' do
+          let(:params) { { names: ['Name 1', 'Name 2'] } }
+
+          it { should be_success }
+
+          it "the errors should be empty" do
+            expect(schema_validation.errors).to be_empty
+          end
+        end
+      end
+    end
+
+    describe 'hash' do
+      subject(:schema_validation) do
+        schema = Dry::Schema.Params do
+          required(:address).hash do
+          end
+        end
+
+        schema.call params
+      end
+
+      context 'negative' do
+        context 'wrong type is passed as value' do
+          let(:params) { { address: 'asd' } }
+
+          it { should be_failure }
+
+          it "the errors should point out the missing key" do
+            expect(schema_validation.errors.to_h).to include(:address)
+          end
+        end
+      end
+
+      context 'positive' do
+        context 'present key' do
+          let(:params) { { address: {} }}
+
+          it { should be_success }
+
+          it "the errors should be empty" do
+            expect(schema_validation.errors).to be_empty
+          end
+        end
+      end
+    end
+  end
+
+  describe 'value' do
+  end
+
+  describe 'nested schemas' do
   end
 
   describe 'schema reuse' do
