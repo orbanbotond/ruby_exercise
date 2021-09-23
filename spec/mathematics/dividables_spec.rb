9a14517498f6ffe6bc5052c1864c2bb11116c020
Orban Botond <orbanbotond@gmail.com>
2021-09-23 19:11:30 +0300

    Beautify the dividable by 3

--- spec/mathematics/dividables_spec.rb
+++ spec/mathematics/dividables_spec.rb
@@ -2,16 +2,11 @@ require 'spec_helper'
 
 describe 'Dividables' do
   describe 'by 3' do
-    context 'when the last 3 numbers are dividable by 3' do
+    context 'when the sum of their digits are dividable by 3' do
       specify 'the number is dividable by 3' do
-        dividables = (1..99).to_a.select{|x| x%3 === 0}
-        # File.open("dividables_by_three.marshal", "w"){|to_file| Marshal.dump(dividables, to_file)}
-        # dividables = File.open("#{__dir__}/dividables_by_three.marshal", "r"){|from_file| Marshal.load(from_file)}
-
-        (1000..2000).to_a.select{|x|dividables.include?(x.to_s[-2..-1].to_i)}.each do|number|
+        dividables = (1000..2000).to_a.select{|number|number.to_s.chars.map(&:to_i).reduce(0){|accumulator, digit| digit+accumulator}%3==0}
+        dividables.each do |number|
           expect(number%3).to eq(0)
-        rescue RSpec::Expectations::ExpectationNotMetError
-          puts "number:`#{number}` should be dividable by `3`"
         end
       end
     end
